5a54b5e10d7350676c1890f97d0f77b820496edc
Renan Machado <renan.mav@hotmail.com>
2020-03-19 14:04:07 -0300

    run detox locally

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -4,6 +4,6 @@ describe('Example', () => {
   });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.text('Step One'))).toBeVisible();
+    await expect(element(by.text('Welcome to React'))).toBeVisible();
   });
 });
